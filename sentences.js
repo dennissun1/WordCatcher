372912a14bf944cd7dcc3d3faf7198abe98d5bdf
brittneyhklee <hakyung@live.unc.edu>
2017-04-17 12:32:30 -0400

    Update sentences.js
    testing to see if Editing on github works

--- sentences.js
+++ sentences.js
@@ -1,7 +1,8 @@
-var numSents = 2;//number of sentences
+var numSents = 3;//number of sentences
 
 var sents = [    //array of sentences, note that part1 ends with space and part2 starts with a space
     {part1:"Yesterday, the dog ", part2:" around in the park.", w1:"ran", w2:"barked", w3:"fluffy", correct:"ran"},
     {part1:"Tomorrow, I will ", part2:" all of my homework.", w1:"complicated", w2:"complete", w3:"compare", correct:"complete"}
+    {part1:"Katie is a ", part2:" dancer.", w1:"graceful", w2:"run", w3:"annoy", correct:"graceful"},
            ];
 
