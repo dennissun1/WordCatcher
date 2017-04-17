37903da19315389e53bf21021ac34923b0e6d023
brittneyhklee <hakyung@live.unc.edu>
2017-04-17 13:03:03 -0400

    Update sentences.js

--- sentences.js
+++ sentences.js
@@ -1,8 +1,13 @@
-var numSents = 3;//number of sentences
+var numSents = 7;//number of sentences
 
 var sents = [    //array of sentences, note that part1 ends with space and part2 starts with a space
     {part1:"Yesterday, the dog ", part2:" around in the park.", w1:"ran", w2:"barked", w3:"fluffy", correct:"ran"},
     {part1:"Tomorrow, I will ", part2:" all of my homework.", w1:"complicated", w2:"complete", w3:"compare", correct:"complete"}
     {part1:"Katie is a ", part2:" dancer.", w1:"graceful", w2:"run", w3:"annoy", correct:"graceful"},
+    {part1:"The red apple is ", part2:" on that tree.", w1:"growing", w2:"grew", w3:"grow", correct:"growing"},
+    {part1:"The children are ", part2:" in the playground.", w1:"eat", w2:"playing", w3:"sound", correct:"playing"},
+    {part1:"The new car is very ", part2:".", w1:"rickety", w2:"broken", w3:"shiny", correct:"shiny"},
+    {part1:"The cake at the store was so ", part2:", I bought another one.", w1:"expensive", w2:"cost", w3:"cheap", correct:"cheap"},
+
            ];
 
