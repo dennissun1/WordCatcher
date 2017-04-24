840d1c781d2b9857be6e93fa30445e77c2e52f8f
twli <tim.li7747@gmail.com>
2017-04-24 16:25:01 -0400

    Update WCG1Script.js

--- WCG1Script.js
+++ WCG1Script.js
@@ -2,8 +2,8 @@ window.addEventListener("keyup", dealWithKeyboard, false);
 var hlitedCol = {name:"midCol", left:"leftCol", right:"rightCol"};
 var s, total_num_sents, rng;
 var score = 10;
-var keypress = new Audio();
-keypress = "click.mp3";
+var keypress = new Audio("click.mp3);
+
 
 function dealWithKeyboard(e) {
     if(e.keyCode == 37) {//left arrowkey
