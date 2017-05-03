c13583c462a04fb2edea7a268ab369bf06cf06bb
brittneyhklee <hakyung@live.unc.edu>
2017-05-03 19:53:58 -0400

    Update WCG1Script.js

--- WCG1Script.js
+++ WCG1Script.js
@@ -3,7 +3,7 @@ var hlitedCol = {name:"midCol", left:"leftCol", right:"rightCol"};
 var s, total_num_sents, rng;
 var score = 0, iter=-1;
 var background = new Audio("background.mp3");
-background.volume = 0.7;
+background.volume = 0.4;
 var keypress = new Audio("click.mp3");
 var correct = new Audio("correct.mp3");
 var wrong = new Audio("wrong.mp3");
